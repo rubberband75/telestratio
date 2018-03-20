272a11d0092d452b61b358a597d4a64a63d62554
Chandler Childs <chandler@sollenaturals.com>
2018-03-20 15:03:24 -0600

    add mobile support

--- server.js
+++ server.js
@@ -11,24 +11,24 @@ server.listen(process.env.PORT || 4000);
 console.log('Server running...');
 
 
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', function (socket) {
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length);
 
     // New Game
-    socket.on('new game', function(data, callback){
+    socket.on('new game', function (data, callback) {
         var gameName = "game" + games.length
-        var game = {name: gameName, users:[socket], usernames:[], messages:[]}
+        var game = { name: gameName, users: [socket], usernames: [], messages: [] }
         games.push(game)
         socket.game = game
-        
+
         socket.emit('game joined', gameName);
     })
 
     // Join Game
-    socket.on('join game', function(data){
+    socket.on('join game', function (data) {
         var game = games.find(x => x.name === data);
-        if(game){
+        if (game) {
             socket.game = game
             socket.game.users.push(socket)
             socket.emit('game found', socket.game.name);
@@ -39,7 +39,7 @@ io.sockets.on('connection', function(socket){
     });
 
     // New User
-    socket.on('new user', function(data, callback){
+    socket.on('new user', function (data, callback) {
         callback(true);
         socket.username = data;
         users.push(socket.username);
@@ -48,44 +48,48 @@ io.sockets.on('connection', function(socket){
     })
 
     // Disconnect
-    socket.on('disconnect', function(data){
-        if(socket.game) socket.game.usernames.splice(socket.game.usernames.indexOf(socket.username), 1);
+    socket.on('disconnect', function (data) {
+        if (socket.game) socket.game.usernames.splice(socket.game.usernames.indexOf(socket.username), 1);
 
         updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
-        if(socket.game) socket.game.users.splice(socket.game.users.indexOf(socket), 1);        
+        if (socket.game) socket.game.users.splice(socket.game.users.indexOf(socket), 1);
         console.log('Dissconnected: %s sockets connected', connections.length);
     });
 
     // Send Message
-    socket.on('send message', function(data){
-        for(var i = 0; i <  socket.game.users.length; i++){
-            var msg = {msg: data, user: socket.username, type:'txt'};
-            socket.game.messages.push(msg);
-            socket.game.users[i].emit('new message', msg);
+    socket.on('send message', function (data) {
+        if (socket.game) {
+            for (var i = 0; i < socket.game.users.length; i++) {
+                var msg = { msg: data, user: socket.username, type: 'txt' };
+                socket.game.messages.push(msg);
+                socket.game.users[i].emit('new message', msg);
+            }
         }
     })
 
     // Send Drawing
-    socket.on('send drawing', function(data){
-        for(var i = 0; i <  socket.game.users.length; i++){
-            var msg = {msg: data, user: socket.username, type:'img'};
-            socket.game.messages.push(msg);
-            socket.game.users[i].emit('new message', msg);
+    socket.on('send drawing', function (data) {
+        if (socket.game) {
+            for (var i = 0; i < socket.game.users.length; i++) {
+                var msg = { msg: data, user: socket.username, type: 'img' };
+                socket.game.messages.push(msg);
+                socket.game.users[i].emit('new message', msg);
+            }
         }
     })
 
-    function retrieveMessages(){
-        if(socket.game){
-            for(var i = 0; i <  socket.game.messages.length; i++){
+    function retrieveMessages() {
+        if (socket.game) {
+            for (var i = 0; i < socket.game.messages.length; i++) {
                 socket.emit('new message', socket.game.messages[i]);
             }
         }
     }
 
-    function updateUsernames(){
-        if(socket.game){
-            for(var i = 0; i <  socket.game.users.length; i++){
+    function updateUsernames() {
+        if (socket.game) {
+            for (var i = 0; i < socket.game.users.length; i++) {
                 socket.game.users[i].emit('get users', socket.game.usernames);
             }
         }
